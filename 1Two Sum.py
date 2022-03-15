42fbc76f762ca2fd6ace98466e67f2ce746a5b67
ManasaR08 <manasar259@gmail.com>
2022-03-15 08:09:35 -0700

    updated

--- 1Two Sum.py
+++ 1Two Sum.py
@@ -1,10 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dct = {}
+        result = []
         for i in range(len(nums)):
             dct[nums[i]] = i
         for i in range(len(nums)):
-            value = target - nums[i]
-            if value in nums:
-                if nums.index(value) != i:
-                    return i , nums.index(value)
+            if(target-nums[i] in dct.keys()):
+                if(dct[target-nums[i]]!=i):
+                    result.append(i)
+                    result.append(dct[target-nums[i]])
+                    break
+        return result
+        
